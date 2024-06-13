1d112b3e61d4b04047739ffb5be6a34fc8fa1a3f
Hector Diaz <hector.diaz.b99@gmail.com>
2024-06-12 22:24:40 -0400

    Creacion de Menu de navegacion

---
+++ ClienteJeshu/script.js
@@ -0,0 +1,8 @@
+const ham = document.querySelector('.ham');
+const bar = document.querySelector('#bar');
+const barras = document.querySelectorAll('.ham span');
+
+ham.addEventListener('click', () =>{
+    bar.classList.toggle('activado');
+    barras.forEach(child => {child.classList.toggle('animado')})
+})
